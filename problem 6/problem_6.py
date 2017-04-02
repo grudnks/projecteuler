d4ac333135c381cbafeae7e5cecbf8977434e7a8
Konstantin <kost159@yandex.ru>
2017-04-02 12:01:32 +0300

    Problem 6

--- problem 6/problem_6.py
+++ problem 6/problem_6.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+def get_sum(n):
+	return sum([ x for x in range(1, n+1) ])
 
-# Answer: 
+def get_sq_sum(n):
+	return sum([ x**2 for x in range(1, n+1) ])
+
+def get_sum_sq_dif(n):
+	return get_sum(n)**2 - get_sq_sum(n)
+
+print(get_sum_sq_dif(100))
+# Answer: 25164150
